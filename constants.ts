29ae35caf227697587969ca98a0efa32e6f3f6c4
Alexandre Ferreira <cadastrandoweb@gmail.com>
2025-12-30 18:50:58 -0300

    Altera o preço do produto principal de R para R

--- constants.ts
+++ constants.ts
@@ -3,7 +3,7 @@ import { Product } from './types';
 export const MAIN_PRODUCT: Product = {
   id: 'main-prod-001',
   name: 'Mestres do Tráfego Vitalício',
-  price: 67.00,
+  price: 47.00,
   // OBS: Substitua a URL abaixo pelo link direto da imagem que você enviou após hospedá-la
   image: '/mestres.webp'
 };
